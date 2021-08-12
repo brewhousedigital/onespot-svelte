0a472b9779bead4f4841d94091631d238b623588
Brewhouse Digital <zach@brewhouse.digital>
2021-08-12 14:29:52 -0500

    Improved the search page.

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -2,8 +2,10 @@
     import {onMount} from "svelte";
 
     let searchTerm = "";
-    let results = [];
     let OSD = [];
+    let filteredResults = [];
+    let results = [];
+    let archiveResults = [];
 
     onMount(() => {
         OSD = JSON.parse(localStorage.getItem("OSD"))
@@ -13,34 +15,37 @@
         //console.log("searchTerm", searchTerm);
         //console.log("OSD", OSD);
 
-        if(searchTerm.length > 3) {
-            results = OSD.filter(item => {
+        if(searchTerm.length > 2) {
+            filteredResults = OSD.filter(item => {
                 // Search the title
                 if(item.title) {
-                    if(item.title.includes(searchTerm)) {return item;}
+                    if(item.title.toLowerCase().includes(searchTerm)) {return item;}
                 }
 
                 // Search the category
                 if(item.category) {
-                    if (item.category.includes(searchTerm)) {return item;}
+                    if (item.category.toLowerCase().includes(searchTerm)) {return item;}
                 }
 
                 // Search the content
                 if(item.content) {
-                    if (item.content.includes(searchTerm)) {return item;}
+                    if (item.content.toLowerCase().includes(searchTerm)) {return item;}
                 }
 
                 // Search the url
                 if(item.url) {
-                    if (item.url.includes(searchTerm)) {return item;}
+                    if (item.url.toLowerCase().includes(searchTerm)) {return item;}
                 }
-            })
+            });
+
+            // Filter out archived and not archived
+            archiveResults = filteredResults.filter(item => item.archived);
+            results = filteredResults.filter(item => !item.archived);
         } else {
             // Reset list
             results = [];
+            archiveResults = [];
         }
-
-        console.log("results", results)
     }
 </script>
 
@@ -67,10 +72,28 @@
            placeholder="Search anything...">
 </div>
 
-<div class="list-group">
+
+{#if searchTerm.length > 2}
+    <h2>Active Pages</h2>
+<div class="list-group pb-5 border-bottom mb-5">
     {#each results as result}
-        <a href="{result.url}" class="list-group-item list-group-item-action">{result.title}</a>
+        <a href="{result.url}" class="list-group-item list-group-item-action">{result.title} <i class="bi bi-link-45deg"></i></a>
     {:else}
-        <p>No results yet!</p>
+        <p>No active pages with "{searchTerm}" were found</p>
     {/each}
 </div>
+{:else}
+    <p>Type more than 2 characters to see results</p>
+{/if}
+
+
+{#if searchTerm.length > 2}
+    <h2>Archived Pages</h2>
+<div class="list-group">
+    {#each archiveResults as result}
+        <a href="{result.url}" class="list-group-item list-group-item-action">{result.title} <i class="bi bi-link-45deg"></i></a>
+    {:else}
+        <p>No archive pages with "{searchTerm}" were found</p>
+    {/each}
+</div>
+{/if}
